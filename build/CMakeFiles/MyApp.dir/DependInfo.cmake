ef63e67e92e369edbff9854f681e1c2329f2a6d3
ShihaoWang <sw309@duke.edu>
2020-05-29 10:03:56 -0500

    Commit for Backup

---
+++ build/CMakeFiles/MyApp.dir/DependInfo.cmake
@@ -0,0 +1,85 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/src/ContactController.cpp" "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/build/CMakeFiles/MyApp.dir/src/ContactController.cpp.o"
+  "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/src/ContactInfoLoader.cpp" "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/build/CMakeFiles/MyApp.dir/src/ContactInfoLoader.cpp.o"
+  "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/src/ContactPlanning.cpp" "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/build/CMakeFiles/MyApp.dir/src/ContactPlanning.cpp.o"
+  "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/src/ContactPlanningInner.cpp" "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/build/CMakeFiles/MyApp.dir/src/ContactPlanningInner.cpp.o"
+  "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/src/ConvexPolyhedron.cpp" "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/build/CMakeFiles/MyApp.dir/src/ConvexPolyhedron.cpp.o"
+  "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/src/EndEffectorOrientation.cpp" "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/build/CMakeFiles/MyApp.dir/src/EndEffectorOrientation.cpp.o"
+  "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/src/EnviGeometry.cpp" "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/build/CMakeFiles/MyApp.dir/src/EnviGeometry.cpp.o"
+  "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/src/EuclideanInterpolation.cpp" "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/build/CMakeFiles/MyApp.dir/src/EuclideanInterpolation.cpp.o"
+  "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/src/InitialConfigurationOptimization.cpp" "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/build/CMakeFiles/MyApp.dir/src/InitialConfigurationOptimization.cpp.o"
+  "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/src/InitialStateOptimization.cpp" "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/build/CMakeFiles/MyApp.dir/src/InitialStateOptimization.cpp.o"
+  "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/src/Main.cpp" "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/build/CMakeFiles/MyApp.dir/src/Main.cpp.o"
+  "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/src/ReachabilityMap.cpp" "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/build/CMakeFiles/MyApp.dir/src/ReachabilityMap.cpp.o"
+  "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/src/RobotStateFiler.cpp" "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/build/CMakeFiles/MyApp.dir/src/RobotStateFiler.cpp.o"
+  "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/src/RobotUtils.cpp" "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/build/CMakeFiles/MyApp.dir/src/RobotUtils.cpp.o"
+  "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/src/SimulationRelated.cpp" "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/build/CMakeFiles/MyApp.dir/src/SimulationRelated.cpp.o"
+  "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/src/SimulationTest.cpp" "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/build/CMakeFiles/MyApp.dir/src/SimulationTest.cpp.o"
+  "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/src/Splines.cpp" "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/build/CMakeFiles/MyApp.dir/src/Splines.cpp.o"
+  "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/src/StabilizingControllerContact.cpp" "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/build/CMakeFiles/MyApp.dir/src/StabilizingControllerContact.cpp.o"
+  "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/src/TouchDownConfig.cpp" "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/build/CMakeFiles/MyApp.dir/src/TouchDownConfig.cpp.o"
+  "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/src/TouchDownPlanning.cpp" "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/build/CMakeFiles/MyApp.dir/src/TouchDownPlanning.cpp.o"
+  "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/src/TransientOptimization.cpp" "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/build/CMakeFiles/MyApp.dir/src/TransientOptimization.cpp.o"
+  "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/src/TransientTrajectory.cpp" "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/build/CMakeFiles/MyApp.dir/src/TransientTrajectory.cpp.o"
+  "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/src/WholeBodyEstimation.cpp" "/home/motion/Desktop/Online-Contact-Planning-for-Fall-Mitigation/build/CMakeFiles/MyApp.dir/src/WholeBodyEstimation.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ASSIMP_MAJOR_VERSION=3"
+  "ENABLE_NON_DETERMINISTIC_PARALLELISM"
+  "HAVE_ASSIMP=1"
+  "HAVE_CURL=1"
+  "HAVE_FREE_IMAGE=1"
+  "HAVE_GLEW=1"
+  "HAVE_GLPK=1"
+  "HAVE_GLUI=1"
+  "HAVE_GLUT=1"
+  "HAVE_LOG4CXX=1"
+  "HAVE_PANGOLIN"
+  "HAVE_ROS=1"
+  "HAVE_TINYXML=1"
+  "LOG4CXX_STATIC"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_WIDGETS_LIB"
+  "TIXML_USE_STL"
+  "USE_CPP_THREADS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/motion/Klampt/Cpp/Dependencies/glui-2.36/src/include"
+  "/home/motion/Klampt/Cpp/Dependencies"
+  "/home/motion/Klampt/Cpp/Dependencies/ode-0.14/include"
+  "/opt/ros/kinetic/include"
+  "/home/motion/Klampt/include"
+  "/home/motion/Klampt/Cpp"
+  "/home/motion/Klampt/Cpp/Dependencies/KrisLibrary"
+  "/home/motion/snopt7/include"
+  "/home/motion/eigen-3.3.5"
+  "/home/motion/cilantro/include"
+  "/home/motion/Pangolin/include"
+  "/home/motion/Pangolin/build/src/include"
+  "/home/motion/Desktop/Polytopic-Viability-Kernel-Plus/src"
+  "/home/motion/gurobi811/linux64/include"
+  "/home/motion/SplineLibrary"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++-64"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/MyApp.dir/cmake_clean.cmake
@@ -0,0 +1,32 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyApp.dir/src/Main.cpp.o"
+  "CMakeFiles/MyApp.dir/src/ContactInfoLoader.cpp.o"
+  "CMakeFiles/MyApp.dir/src/ContactPlanning.cpp.o"
+  "CMakeFiles/MyApp.dir/src/ContactPlanningInner.cpp.o"
+  "CMakeFiles/MyApp.dir/src/EnviGeometry.cpp.o"
+  "CMakeFiles/MyApp.dir/src/ReachabilityMap.cpp.o"
+  "CMakeFiles/MyApp.dir/src/RobotStateFiler.cpp.o"
+  "CMakeFiles/MyApp.dir/src/RobotUtils.cpp.o"
+  "CMakeFiles/MyApp.dir/src/InitialStateOptimization.cpp.o"
+  "CMakeFiles/MyApp.dir/src/InitialConfigurationOptimization.cpp.o"
+  "CMakeFiles/MyApp.dir/src/SimulationTest.cpp.o"
+  "CMakeFiles/MyApp.dir/src/ConvexPolyhedron.cpp.o"
+  "CMakeFiles/MyApp.dir/src/StabilizingControllerContact.cpp.o"
+  "CMakeFiles/MyApp.dir/src/TransientTrajectory.cpp.o"
+  "CMakeFiles/MyApp.dir/src/TransientOptimization.cpp.o"
+  "CMakeFiles/MyApp.dir/src/Splines.cpp.o"
+  "CMakeFiles/MyApp.dir/src/ContactController.cpp.o"
+  "CMakeFiles/MyApp.dir/src/WholeBodyEstimation.cpp.o"
+  "CMakeFiles/MyApp.dir/src/TouchDownConfig.cpp.o"
+  "CMakeFiles/MyApp.dir/src/TouchDownPlanning.cpp.o"
+  "CMakeFiles/MyApp.dir/src/EuclideanInterpolation.cpp.o"
+  "CMakeFiles/MyApp.dir/src/EndEffectorOrientation.cpp.o"
+  "CMakeFiles/MyApp.dir/src/SimulationRelated.cpp.o"
+  "MyApp.pdb"
+  "MyApp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
